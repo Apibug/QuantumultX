8013871de47c60293f92b2d19c0d04e7d20f0519
Apibug <9147218@qq.com>
2024-03-20 01:38:09 +0800

    Update cpdq.js

--- cpdq.js
+++ cpdq.js
@@ -14,13 +14,7 @@ hostname = dq.xiangha.com,apiios_dq.xiangha.com
 
 *******************************/
 
-var _0x1dc272=_0x2e482c(this,function(){
-	var _0x149624=function(){
-		var _0x3d66a6=_0x149624.constructor('return /" + this + "/')().compile('^([^ ]+( +[^ ]+)+)+[^ ]}');
-		return!_0x3d66a6.test(_0x1dc272);
-	};
-	return _0x149624();
-});
-_0x1dc272();
+
+
 body=$response.body.replace(/\"is_vip":\d/g,'"is_vip":2').replace(/\"is_open_vip":\d/g,'"is_open_vip":1').replace(/\"serverTime":\d+/g,'"serverTime":4093982700').replace(/\"isShow":"\d"/g,'"isShow":"2"').replace(/\"nickName":".*?"/g,'"nickName":"Apibug破解"');
 $done({'body':body});
