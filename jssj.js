0fe1f0cef8ef6eafc084f16cb6e8081eb7f89393
Apibug <9147218@qq.com>
2024-04-24 17:44:13 +0800

    Update jssj.js

--- jssj.js
+++ jssj.js
@@ -12,14 +12,14 @@ var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
 
-const vip = '/vip';
+const vip = '=vip';
 
 
 if (url.indexOf(vip) != -1) {
 obj.data.nick = "Apibug破解";
+obj.data.vip_str ="Apibug破解";
 obj.data.avatar = "http://q2.qlogo.cn/headimg_dl?dst_uin=9147218&spec=100";
-obj.data.is_valid_user = "2099-12-21 23:59:59";
-obj.data.vip_expiration = 8888888888;
+obj.data.vip_expiration = "2099-12-29 23:59:59";
 obj.data.vip_level = 2;
 obj.data.level = 999;
 
