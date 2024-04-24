c7ad13ec48129875008200e7682583af2ed2202d
Apibug <9147218@qq.com>
2024-04-24 17:53:26 +0800

    Update jssj.js

--- jssj.js
+++ jssj.js
@@ -12,19 +12,17 @@ var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
 
-const vip = '/userinfo';
-
+const vip = 'userinfo?show=vip'; // 修改匹配规则，确保匹配到含有指定参数的URL
 
 if (url.indexOf(vip) != -1) {
-obj.data.nick = "Apibug破解";
-obj.data.vip_str ="Apibug破解";
-obj.data.avatar = "http://q2.qlogo.cn/headimg_dl?dst_uin=9147218&spec=100";
-obj.data.vip_expiration = "2099-12-29 23:59:59";
-obj.data.vip_level = 2;
-obj.data.level = 999;
+    obj.data.nick = "Apibug破解";
+    obj.data.vip_str = "Apibug破解";
+    obj.data.avatar = "http://q2.qlogo.cn/headimg_dl?dst_uin=9147218&spec=100";
+    obj.data.vip_expiration = "2099-12-29 23:59:59";
+    obj.data.vip_level = 2;
+    obj.data.level = 999;
 
-	body = JSON.stringify(obj);
+    body = JSON.stringify(obj);
 }
-
 
 $done({body});
